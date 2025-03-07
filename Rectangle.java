533b76ca7a4e129842c43c71dc629c44d8176eb1
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-07 21:37:26 +0530

    Implemented calculateVolume() for Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -10,3 +10,8 @@ class Rectangle extends Shape {
     public double calculatePerimeter() {
         return 2 * (dim_one + dim_two);
     }
+
+     public double calculateVolume() {
+        return 0; // No volume for a rectangle
+    }
+}
