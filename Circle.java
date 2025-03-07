f5d589ce0925e762c5eac051243abdc294e99893
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-07 21:33:23 +0530

    Implemented calculateArea() for Circle

--- Circle.java
+++ Circle.java
@@ -3,3 +3,7 @@ class Circle extends Shape {
     public Circle(double radius) {
         super(radius, 0, 0, 0);
     }
+
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
