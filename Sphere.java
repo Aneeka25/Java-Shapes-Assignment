ab83c94d95d44094226eae4062240ea463b19a37
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-07 21:41:48 +0530

    Implemented calculateArea() for Sphere

--- Sphere.java
+++ Sphere.java
@@ -3,3 +3,7 @@ class Sphere extends Shape {
     public Sphere(double radius) {
         super(radius, 0, 0, 0);
     }
+
+     public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
